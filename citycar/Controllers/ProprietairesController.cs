56a5f9c35bad16bbeb18cf727889263065db2e4d
Yao Serge François D'assise Yéboué <2667595@collegelacite.ca>
2021-04-19 07:59:04 -0400

    affichage en fonction de l'authentification

---
+++ citycar/Controllers/ProprietairesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using citycar.Data;
+using citycar.Models;
+
+namespace citycar.Controllers
+{
+    public class ProprietairesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProprietairesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Proprietaires
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Proprietaire.ToListAsync());
+        }
+
+        // GET: Proprietaires/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var proprietaire = await _context.Proprietaire
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proprietaire == null)
+            {
+                return NotFound();
+            }
+
+            return View(proprietaire);
+        }
+
+        // GET: Proprietaires/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proprietaires/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nom,Prenom")] Proprietaire proprietaire)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(proprietaire);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proprietaire);
+        }
+
+        // GET: Proprietaires/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var proprietaire = await _context.Proprietaire.FindAsync(id);
+            if (proprietaire == null)
+            {
+                return NotFound();
+            }
+            return View(proprietaire);
+        }
+
+        // POST: Proprietaires/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nom,Prenom")] Proprietaire proprietaire)
+        {
+            if (id != proprietaire.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(proprietaire);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProprietaireExists(proprietaire.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proprietaire);
+        }
+
+        // GET: Proprietaires/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var proprietaire = await _context.Proprietaire
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proprietaire == null)
+            {
+                return NotFound();
+            }
+
+            return View(proprietaire);
+        }
+
+        // POST: Proprietaires/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var proprietaire = await _context.Proprietaire.FindAsync(id);
+            _context.Proprietaire.Remove(proprietaire);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProprietaireExists(int id)
+        {
+            return _context.Proprietaire.Any(e => e.Id == id);
+        }
+    }
+}
