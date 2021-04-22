cc92a084053a0ede7335b5f5ca6e0a38cc443b1b
messan apaloo <2684447@edu.collegelacite.ca>
2021-04-22 02:24:57 -0400

    Block commentaire (100% A nouveau)

--- citycar/Controllers/CommentairesController.cs
+++ citycar/Controllers/CommentairesController.cs
@@ -27,7 +27,6 @@ namespace citycar.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,TextCommentaire,Date")] Commentaire commentaire, Voiture voiture)
         {
-            commentaire.TextCommentaire = StripHTML(commentaire.TextCommentaire);
             
             commentaire.Voiture = _context.Voitures.FirstOrDefault(m => m.Id == voiture.Id);
             commentaire.Id = 0;
@@ -57,10 +56,7 @@ namespace citycar.Controllers
         {
             return _context.Commentaire.Any(e => e.Id == id);
         }
-        public static string StripHTML(string input)
-        {
-            return Regex.Replace(input, "<.*?>", String.Empty);
-        }
+        
 
     }
 }
