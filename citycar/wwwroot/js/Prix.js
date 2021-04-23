3037ffe7796e46d7ab771f252605558bc53b94b0
NAKOULMA Wendyam Christelle <2674822@edu.collegelacite.ca>
2021-04-22 21:19:29 -0500

    Validation prix

---
+++ citycar/wwwroot/js/Prix.js
@@ -0,0 +1,16 @@
+﻿
+
+
+function CheckDecimal() {
+    var prix = form.prix.value;
+    var decimal = /^?[0-9]+\.[0-9]/;
+    if (prix.value.match(decimal)) {
+        
+        return true;
+    }
+    else {
+       
+        return false;
+    }
+}
+
