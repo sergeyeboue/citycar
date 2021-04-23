abe8155a9b15f0c2d77e190c78f8b6b6377c41c9
NAKOULMA Wendyam Christelle <2674822@edu.collegelacite.ca>
2021-04-22 19:30:42 -0500

    Validation sur image edit

--- citycar/wwwroot/js/Validation.js
+++ citycar/wwwroot/js/Validation.js
@@ -1,1 +1,21 @@
-﻿
+﻿//VALIDATION sur l'extension de l'image edit
+
+function validatedit() {
+
+    var file = form.file.value;
+    var reg = /(.*?)\.(jpg|jpeg|png)$/;
+
+    if (file == '') {
+        return true;
+    }
+    else {
+        if (!file.match(reg)) {
+            alert("L'image doit respecter le format: jpg, jpeg et png");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+}
